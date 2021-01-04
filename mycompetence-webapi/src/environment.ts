5057936a8641e7e24ab5552afe02ad5dbb0f8663
Shynixn <shynixn@email.com>
2021-01-04 21:30:04 +0100

    Implemented nosql database connection.

---
+++ mycompetence-webapi/src/environment.ts
@@ -0,0 +1,2 @@
+// Change it for development. It is important to name it "DATABASE_SECRET_TOKEN".
+export const DATABASE_SECRET = "DATABASE_SECRET_TOKEN"
--- mycompetence-webapi/src/person.ts
+++ mycompetence-webapi/src/person.ts
@@ -1,5 +1,6 @@
 import {APIGatewayEvent, Context} from 'aws-lambda'
 import {Person} from "./entity/person.entity";
+import {DatabaseService} from "./impl/database.service";
 
 // @ts-ignore
 exports.handler = async function (event: APIGatewayEvent, context: Context) {
@@ -19,6 +20,23 @@ exports.handler = async function (event: APIGatewayEvent, context: Context) {
     }
 
     // GET http://localhost:8888/api/person
+
+    // Don't forget to set the DATABASE_SECRET key while developing. Do not check DATABASE_SECRET into the source control!
+    let databaseService = new DatabaseService()
+
+    // Add to database
+    let addedItem = await databaseService.add("users", {
+        id: "",
+        firstName: "Franz",
+        lastName: "Paul"
+    })
+    console.log("Added item:")
+    console.log(JSON.stringify(addedItem))
+
+    // GetAll from database.
+    let data = await databaseService.getAll("users")
+    console.log("Result:")
+    console.log(JSON.stringify(data))
 
     return {
         statusCode: 200,
