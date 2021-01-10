6938ccb5e6c0014ded5403116618c5a1249ac39c
Henry <ruds@mindcave.com>
2021-01-10 17:18:42 +0100

    Add logic to add approval (sorting in db)

--- mycompetence-webapi/src/entity/competence.entity.ts
+++ mycompetence-webapi/src/entity/competence.entity.ts
@@ -7,4 +7,21 @@ export class Competence implements Identifyable {
     isPublic: boolean
     description: string
     approved: Approval[]
+    countApproved: number = 0
+
+    public addApproval(userId: string) {
+        this.approved.forEach(chk => {
+            if (chk.userId === userId) {
+                // @ts-ignore
+                chk.date = Date.now()
+                
+            } else {
+                var a = new Approval()
+                a.userId = userId
+                // @ts-ignore
+                a.date = Date.now()
+                this.countApproved = this.approved.push(a)
+            }
+        });
+    }
 }
