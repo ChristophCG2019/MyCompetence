c40cfa689277c8cc769a803cb5e29dac678fa16f
OctaAlpha1 <lel.ben@gmx.at>
2021-01-11 13:37:49 +0100

    Added full navbar if user is not logged in

--- mycompetence-frontend/src/app/app.component.ts
+++ mycompetence-frontend/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  loggedIn: boolean;
+
   title = 'mycompetence';
+
+  ngOnInit(): void {
+    this.loggedIn = false;
+  }
 }
