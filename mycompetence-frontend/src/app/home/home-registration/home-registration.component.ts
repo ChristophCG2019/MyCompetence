4215b1012e4a170d18909869e4748f3bfbbddcbe
OctaAlpha1 <lel.ben@gmx.at>
2021-01-13 16:39:30 +0100

    Added reading of registration data

--- mycompetence-frontend/src/app/home/home-registration/home-registration.component.ts
+++ mycompetence-frontend/src/app/home/home-registration/home-registration.component.ts
@@ -17,6 +17,10 @@ export class HomeRegistrationComponent implements OnInit {
   }
 
   register(): void {
+    let username = (document.getElementById('usernameInput') as HTMLInputElement).value;
+    let password = (document.getElementById('passwordInput') as HTMLInputElement).value;
+    let e_mail = (document.getElementById('e-mailInput') as HTMLInputElement).value;
+    let organisation = (document.getElementById('organisationInput') as HTMLInputElement).value;
 
   }
 }
